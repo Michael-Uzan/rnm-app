652847174dc69f9c827370018050a6db63ed11c3
Michael Uzan <michaeluzan@byondxr.com>
2025-07-27 00:25:37 +0300

    add charecter details cmp

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import { AppFooter } from "./components/AppFooter";
 import { AppHeader } from "./components/AppHeader";
 import { HalfSide, MainContainer } from "./components/layout/MainContainer";
 import { CharacterGallery } from "./components/CharacterGallery";
+import { CharacterDetails } from "./components/CharacterDetails";
 
 function App() {
   return (
     <Box>
+      <CharacterDetails />
       <AppHeader />
       <MainContainer>
         <HalfSide>
